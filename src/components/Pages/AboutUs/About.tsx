3b13ec3a55e22589c8a953eea2d9daf1d251553a
CJ <cjfritz9@gmail.com>
2023-05-12 13:39:58 -0600

    changes and additions to various 'about' sections per feedback

--- src/components/Pages/AboutUs/About.tsx
+++ src/components/Pages/AboutUs/About.tsx
@@ -5,7 +5,14 @@ import SeantheBondsman from '../../../assets/SeantheBondsman.webp';
 
 const About: React.FC = () => {
   return (
-    <Box py={['2rem','4rem']} bgColor='Brand.White' px={['1rem', '2rem']}>
+    <Box py={['2rem', '4rem']} bgColor='Brand.White' px={['1rem', '2rem']}>
+      <Heading
+        variant='heroText'
+        color='black'
+        mb='2rem'
+      >
+        Meet Sean the Bondsman
+      </Heading>
       <Flex
         flexDir={['column', 'column', 'column', 'column', 'row']}
         w='100%'
@@ -15,42 +22,46 @@ const About: React.FC = () => {
       >
         <Image
           src={SeantheBondsman}
-          h={['320px', '440px', '540px', '600px']}
-          w={['320px', '440px', '540px', '600px']}
+          h={['320px', '440px', '540px', '640px']}
+          w={['320px', '440px', '540px', '640px']}
         />
-        <Stack w={['320px', '440px', '540px', '600px']}>
-          <Heading
-            variant='iwtTitle'
-            textAlign={['center', 'center', 'center', 'center', 'left']}
-          >
-            Meet Sean the Bondsman
-          </Heading>
+        <Stack w={['320px', '440px', '540px', '600px']} gap='1rem'>
           <Text textAlign={['center', 'center', 'center', 'center', 'left']}>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui saepe
-            odit numquam eius sequi modi eligendi debitis perspiciatis
-            dignissimos harum, ducimus doloremque minima quaerat, nesciunt
-            pariatur? Accusamus illum sed porro. Soluta dolorum assumenda rem
-            consequatur corporis, amet autem veniam error tempora exercitationem
-            laudantium eligendi a omnis odio nostrum suscipit sapiente
-            consectetur! Amet itaque natus dignissimos deserunt deleniti,
-            impedit sit praesentium. Temporibus, illum ut aperiam sunt eveniet
-            nulla placeat quis, eos atque hic autem quae tenetur. Consequatur
-            quis possimus voluptatem officiis, quos sit aspernatur reiciendis,
-            sunt odit ratione eos reprehenderit in? Labore sapiente repellendus
-            magni ea officia ipsa! Dolore quas odit porro veritatis nobis
-            repellendus similique, animi aliquam id magnam, delectus magni fugit
-            expedita et possimus omnis impedit iste, laborum qui! Rem corrupti
-            aperiam iusto, odio eaque aut enim molestiae ipsam qui excepturi
-            dolorum, quia itaque optio? Nobis labore similique, modi vitae
-            tempore consequuntur doloribus, hic voluptates quod possimus harum
-            numquam. Dolorum, voluptatibus excepturi alias assumenda veniam
-            neque sapiente maxime, animi suscipit, rerum id nobis? Aliquam nemo
-            facere esse laboriosam quidem tempore facilis quo doloremque ab?
-            Placeat numquam alias natus deserunt. Impedit quibusdam aliquam
-            suscipit vel numquam, explicabo cumque neque hic consectetur maxime
-            iste quasi amet exercitationem. Laboriosam voluptatibus nihil
-            sapiente, dicta culpa ipsum qui rerum inventore aliquid quod quae
-            optio.
+            I started working in the Bail Bonds industry in 2008. It's something
+            I thoroughly enjoy and thank goodness I married wisely, it takes a
+            special woman to put up with the long days and nights and a husband
+            who is always on call! Before I started in this endeavor I was
+            actually in and out of the Shawnee County DOC myself, from my late
+            teens to my early 30s. I have been clean and sober since January
+            28th, 2005. I have worked with addicts and alcoholics ever since,
+            working on staying clean myself and helping others work toward their
+            recovery as well.
+          </Text>
+          <Text>
+            I've worked as a Case Manager here in Topeka, putting in time with
+            persons who were suicidal and feeling hopeless, and persons in our
+            community struggling with Severe and Persistent Mental Illness,
+            Addiction, and homelessness. I know what it's like to be labeled and
+            marginalized and I know how unfair and incorrect these labels can
+            be, separating us from each other. I've been judged, I've felt
+            hopeless, I got the help I needed and I continue to get that help
+            today.
+          </Text>
+          <Text>
+            I'm unusual in the Bail Bonds world because of this and I'm proud of
+            the continued recovery the Topeka community helps me achieve. I've
+            been in a hole that I didn't know how to get out of, I found a way
+            out. I'm grateful.
+          </Text>
+          <Text>
+            I know we offer a “free consultation”, and I hope to be more
+            comprehensive. Sometimes you're just not sure if it is the right
+            time or right thing to do to bond someone out of jail. While I can't
+            make that decision for you, I can listen and offer a different
+            perspective. Our jails are the single largest housing units for
+            addicts and the mentally ill. Not just criminals, but our friends
+            and family, your loved ones. It's not always an easy decision to
+            make.
           </Text>
         </Stack>
       </Flex>
